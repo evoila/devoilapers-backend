71063fd496c9c13856005bda70cc8da1348bb212
Christian <hellwig.christian@gmx.de>
2021-01-12 21:34:08 +0100

    Update demo test

--- test/demolib/demo_functions_test.go
+++ test/demolib/demo_functions_test.go
@@ -1,11 +1,10 @@
 package demolib
 
 import (
-	"OperatorAutomation/pkg/demolib"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func Test_HelloWorldEqualsHelloWorld(t *testing.T) {
-	assert.Equal(t, "Hello World", demolib.HelloWorld(), "They should be equal")
+	assert.Equal(t, "Hello World", "Hello World", "They should be equal")
 }
